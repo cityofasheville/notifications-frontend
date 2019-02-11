f5b4d0496dc43e2983ef0616ead60af27ed84e23
Melanie Mazanec <melanie.mazanec@gmail.com>
2019-02-11 11:25:06 -0500

    Add edit preferences link

--- src/app/Home.js
+++ src/app/Home.js
@@ -11,10 +11,10 @@ const Home = () => (
       Want to keep up with what's happening in Asheville?  Sign up to receive email notifications!
     </p>
     <p>
-      This is a pilot application to let people sign up for non-emergency alerts based on city data.  Our first topic is new, large-scale development permit applications.  Use the feedback form to tell us what should be next.
+      This is a pilot application to let people anonymously sign up for non-emergency alerts based on city data.  Our first topic is new, large-scale development permit applications.  Use the feedback form to tell us what should be next.
     </p>
     <p>
-      This application does not handle emergency alerts.  You can sign up for those at <a href="https://member.everbridge.net/index/453003085611892#/login">AVL Alert</a>.
+      This application does not handle emergency alerts.  You can sign up for those with <a href="https://member.everbridge.net/index/453003085611892#/login" target="_blank" rel="noopener noreferrer">AVL Alert</a>.
     </p>
     <SignupForm/>
   </div>
--- src/app/SignupForm.js
+++ src/app/SignupForm.js
@@ -4,9 +4,10 @@ import 'app/styles/components/SignupForm.css';
 const SignupForm = () => (
   <div class="signup">
 
-    <div class="form-section centered">
+    <div class="form-section centered email">
       <label>Email: </label>
       <input type="email" name="email" />
+      <a href="edit">Already signed up?  Receive a link to edit your preferences</a>
     </div>
 
     <div class="form-section">
