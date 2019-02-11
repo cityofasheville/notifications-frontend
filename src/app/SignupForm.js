eae019d47f9ce7726ef07085be0b12f2c516268b
Melanie Mazanec <melanie.mazanec@gmail.com>
2019-02-11 14:12:26 -0500

    Mockup

--- src/app/SignupForm.js
+++ src/app/SignupForm.js
@@ -2,22 +2,40 @@ import React from 'react';
 import 'app/styles/components/SignupForm.css';
 
 const SignupForm = () => (
-  <div class="signup">
+  <div className="signup">
 
-    <div class="form-section centered email">
-      <label>Email: </label>
-      <input type="email" name="email" />
+    <div className="form-section centered email">
+      <label htmlFor="email-input">Email: </label>
+      <input type="email" name="email"  id="email-input"/>
       <span>Already signed up?  <a href="edit">Get a link to edit your preferences</a></span>
     </div>
 
-    <div class="form-section">
-      <h2>Notification Topics</h2>
+    <div className="form-section">
+      <h2 className="centered" >Notification Categories</h2>
+      <div>
+        <h3>Large Scale Development</h3>
+        {['All notifications', '28001', '28802', '28803', '28804', '28805', 'Affordable Housing'].map(d => {
+          return (<div className="tag-checkbox" key={d}>
+            <input type="checkbox" id={d} name={d}/>
+            <label htmlFor={d}>{d}</label>
+          </div>)
+        })}
+      </div>
+      <h3>More Categories Coming Soon!</h3>
     </div>
 
-    <div class="form-section centered">
+    <div className="form-section centered">
       <button type="submit" name="Send Verification Email">Send Verification Email</button>
     </div>
   </div>
 );
+
+/*
+// TODO:
+* get all categories
+* for each category, have an all option that checks and grays out the other boxes
+* if the all option is unchecked, leave other boxes checked until user unchecks them
+
+*/
 
 export default SignupForm;
