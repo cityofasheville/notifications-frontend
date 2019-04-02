951e00df52c0c0031e43ee05d89e8712960d28da
Melanie Mazanec <melanie.mazanec@gmail.com>
2019-04-02 12:05:35 -0400

    Differentiate logged in vs not

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import 'app/styles/components/Home.css';
+import { Query } from 'react-apollo';
+import { GET_USER_INFO } from 'template/Queries';
 import SignupForm from './SignupForm';
+import 'app/styles/components/Home.css';
 
-const Home = () => (
-  <div>
-    <h1>
-      Sign up for City Notifications
-    </h1>
-    <p>
-      How do I keep up with what's being built downtown or down the street?  What is this building going to look like?  Who is building it?
-    </p>
-    <p>
-      This application does not handle emergency alerts.  You can sign up for those with <a href="https://member.everbridge.net/index/453003085611892#/login" target="_blank" rel="noopener noreferrer">AVL Alert</a>.
-    </p>
-    <ol>
-      <li>Make an account</li>
-      <li>Choose a location</li>
-      <li>Choose categories and set preferences</li>
-      <li>Save preferences</li>
-    </ol>
-    <SignupForm/>
-  </div>
-);
+const Home = () => {
+  const loggedIn = localStorage.getItem('loggedIn') === 'true';
+  return (
+    <Query
+      query={GET_USER_INFO}
+      fetchPolicy="network-only"
+    >
+      {({ loading, error, data }) => {
+        if (loading) return null;
+        if (error) return <a href="#" className="alert-danger">Login Unavailable</a>;
+        if (loggedIn) {
+          return (<div>
+            <h1>
+              Edit notification preferences for {data.user.email}
+            </h1>
+            <p>
+              This application does not handle emergency alerts.  You can sign up for those with <a href="https://member.everbridge.net/index/453003085611892#/login" target="_blank" rel="noopener noreferrer">AVL Alert</a>.
+            </p>
+            <SignupForm/>
+          </div>
+          );
+        }
+        return (<div>
+          <h1>
+            Sign up for notifications
+          </h1>
+          <p>Want to keep up with what's happening downtown or down the street?</p>
+          <p>Currently we offer notifications about: TODO: GET CATEGORIES HERE</p>
+          <ol>
+            <li>Step one</li>
+            <li>Step two</li>
+            <li>Step three</li>
+          </ol>
+          <p>
+            This application does not handle emergency alerts.  You can sign up for those with <a href="https://member.everbridge.net/index/453003085611892#/login" target="_blank" rel="noopener noreferrer">AVL Alert</a>.
+          </p>
+        </div>
+        );
+      }}
+    </Query>
+  );
+};
 
 export default Home;
