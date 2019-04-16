2b036420e85f99bb061617f7aad779bf6b40d537
Melanie Mazanec <melanie.mazanec@gmail.com>
2019-04-16 11:56:01 -0400

    Handle address submit

--- src/app/SelectLocation.js
+++ src/app/SelectLocation.js
@@ -5,10 +5,10 @@ import { Query } from 'react-apollo';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'app/styles/components/SelectLocation.scss';
 
+const geocoder = L.Control.Geocoder.nominatim();
+
 function getAddressFromCoords(x, y, callback) {
-  const geocoder = L.Control.Geocoder.nominatim();
   geocoder.reverse(L.latLng(x, y), 1, function(result) {
-    console.log(result)
     callback([
       result[0].properties.address.house_number || '',
       result[0].properties.address.road || '',
@@ -18,13 +18,21 @@ function getAddressFromCoords(x, y, callback) {
   });
 }
 
+function getCoordsFromAddress(addressText, callback) {
+  geocoder.geocode(addressText, function(result) {
+    callback(result);
+  })
+}
+
 class SelectLocation extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      addressText: undefined,
       selectedAddress: undefined,
       addressCoords: props.x && props.y ? [props.x, props.y] : undefined,
     }
+    this.handleAddressSubmit = this.handleAddressSubmit.bind(this);
   }
 
   componentWillMount() {
@@ -32,6 +40,7 @@ class SelectLocation extends React.Component {
       getAddressFromCoords(this.state.addressCoords[0], this.state.addressCoords[1], (result) =>
         this.setState({
           selectedAddress: result,
+          addressText: result,
         })
       )
     }
@@ -47,7 +56,34 @@ class SelectLocation extends React.Component {
       (result) => this.setState({
         addressCoords: [ x, y],
         selectedAddress: result,
+        addressText: result,
+        addressPossibilities: null,
       }))
+  }
+
+  handleAddressTyping(newAddressText) {
+    this.setState({
+      addressText: newAddressText,
+    })
+  }
+
+  handleAddressSubmit(e) {
+    e.preventDefault();
+    getCoordsFromAddress(
+      `${this.state.addressText} Asheville North Carolina`,
+      (result) => {
+        if (result.length === 1) {
+          this.setState({
+            addressCoords: [result[0].center.lat, result[0].center.lng],
+            selectedAddress: this.state.addressText,
+            addressPossibilities: null,
+          })
+        }
+        else {
+          this.setState({ addressPossibilities: result })
+        }
+      }
+    )
   }
 
   render() {
@@ -57,17 +93,29 @@ class SelectLocation extends React.Component {
         * if they select one or there is only one result, update preferences, SHOW THAT THEY WERE UPDATED
         * if they unfocus and there are not valid coordinates, make next section tell them to select a valid address
     */
-console.log(this.state)
+    console.log(this.state)
     return (<React.Fragment>
-      <div className="SelectLocation-container">
+      <form className="SelectLocation-container" onSubmit={this.handleAddressSubmit}>
         <label className="SelectLocation-label form-element">Address:</label>
         <input
           className="SelectLocation-input form-element"
           type="text"
           defaultValue={this.state.selectedAddress}
-          onKeyDown={(e) => { console.log(e.target.value) }}
+          onChange={(e) => this.handleAddressTyping(e.target.value)}
         />
-      </div>
+        <button type="submit">Confirm Address</button>
+      </form>
+      {this.state.addressPossibilities && this.state.addressPossibilities.length < 1 &&
+        <div className="alert-danger">No results found.  Please try again.</div>
+      }
+      {this.state.addressPossibilities && this.state.addressPossibilities.length > 0 &&
+        <div>
+          <span>Did you mean one of these?</span>
+          {this.state.addressPossibilities.map(possibility => {
+            return <button key={possibility.properties.place_id}>{possibility.name}</button>
+          })}
+        </div>
+      }
       <div>
         <Map
           center={this.state.addressCoords}
