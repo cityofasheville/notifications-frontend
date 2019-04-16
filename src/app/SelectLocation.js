59f1f0f1450145d0be61e0bda42ee8cf9b282f84
Melanie Mazanec <melanie.mazanec@gmail.com>
2019-04-16 11:16:13 -0400

    Handle map click

--- src/app/SelectLocation.js
+++ src/app/SelectLocation.js
@@ -8,7 +8,13 @@ import 'app/styles/components/SelectLocation.scss';
 function getAddressFromCoords(x, y, callback) {
   const geocoder = L.Control.Geocoder.nominatim();
   geocoder.reverse(L.latLng(x, y), 1, function(result) {
-    callback(result[0]);
+    console.log(result)
+    callback([
+      result[0].properties.address.house_number || '',
+      result[0].properties.address.road || '',
+      result[0].properties.address.city || '',
+      result[0].properties.address.postcode || '',
+    ].join(' ').trim());
   });
 }
 
@@ -17,31 +23,41 @@ class SelectLocation extends React.Component {
     super(props);
     this.state = {
       selectedAddress: undefined,
+      addressCoords: props.x && props.y ? [props.x, props.y] : undefined,
     }
   }
 
   componentWillMount() {
-    if (this.props.x && this.props.y) {
-      getAddressFromCoords(this.props.x, this.props.y, (result) =>
+    if (this.state.addressCoords) {
+      getAddressFromCoords(this.state.addressCoords[0], this.state.addressCoords[1], (result) =>
         this.setState({
-          selectedAddress: [
-            result.properties.address.house_number || '',
-            result.properties.address.road || '',
-            result.properties.address.city || '',
-            result.properties.address.postcode || '',
-          ].join(' '),
+          selectedAddress: result,
         })
       )
     }
   }
 
+  updateCoordsFromMap(x, y) {
+    // check to see if point is in the city with simplicity
+    // if it is not, show error message
+    // if it is, setstate with new coords and address
+    getAddressFromCoords(
+      x,
+      y,
+      (result) => this.setState({
+        addressCoords: [ x, y],
+        selectedAddress: result,
+      }))
+  }
+
   render() {
     /*
       TODO: as they type (debounce this):
-        check simplicity for results and show dropdown
-        if they select one or there is only one result, update preferences
-        if they click the map, update preferences
+        * check simplicity for results and show dropdown
+        * if they select one or there is only one result, update preferences, SHOW THAT THEY WERE UPDATED
+        * if they unfocus and there are not valid coordinates, make next section tell them to select a valid address
     */
+console.log(this.state)
     return (<React.Fragment>
       <div className="SelectLocation-container">
         <label className="SelectLocation-label form-element">Address:</label>
@@ -54,15 +70,15 @@ class SelectLocation extends React.Component {
       </div>
       <div>
         <Map
-          center={[this.props.x, this.props.y]}
+          center={this.state.addressCoords}
           zoom={14}
-          onClick={e => console.log(e.latlng)}
+          onClick={e => this.updateCoordsFromMap(e.latlng.lat, e.latlng.lng)}
         >
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
           />
-          <Marker position={[this.props.x, this.props.y]}>
+          <Marker position={this.state.addressCoords}>
             <Popup>{this.state.selectedAddress}</Popup>
           </Marker>
         </Map>
