c215a48bf544b6f83fe9198ce2d8b4bc03e61ed8
Melanie Mazanec <melanie.mazanec@gmail.com>
2019-07-16 14:53:33 -0400

    Disable eslint on paragraphs

--- src/app/UnauthenticatedLanding.js
+++ src/app/UnauthenticatedLanding.js
@@ -8,6 +8,7 @@ const UnauthenticatedLanding = () => (
     <h1>
       Sign up for notifications
     </h1>
+    {/* eslint-disable-next-line */}
     <p id="intro">Want to keep up with what's happening downtown or down the street?  Trying to get more involved with your local government?  Sign up to receive notifications!</p>
     <ul>
       <li>
@@ -15,6 +16,7 @@ const UnauthenticatedLanding = () => (
           <TextPuck text="1" />
         </div>
         <div className="list-item-title">Log in or make an account</div>
+        {/* eslint-disable-next-line */}
         <p>Log in with Google or create a new account.  Use the same or a different email address than the one you use for other city applications.  This data is kept separately to protect your privacy.</p>
       </li>
       <li>
@@ -29,6 +31,7 @@ const UnauthenticatedLanding = () => (
           <TextPuck text="3" />
         </div>
         <div className="list-item-title">Choose which notifications you want to get</div>
+        {/* eslint-disable-next-line */}
         <p>This program is in its pilot phase.  Currently we offer notifications about new, large-scale permit applications only.  Tell us about your interests or what we can do to improve this tool.</p>
       </li>
     </ul>
