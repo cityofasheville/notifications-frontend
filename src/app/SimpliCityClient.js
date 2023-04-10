db47d63a386117b13ece9657187daeb951d36f7f
Jon Wilson <jontwilson@gmail.com>
2023-04-10 11:50:15 -0400

    simplicity server

--- src/app/SimpliCityClient.js
+++ src/app/SimpliCityClient.js
@@ -4,15 +4,16 @@ import { createHttpLink } from 'apollo-link-http';
 
 
 let SIMPLICITY_SERVER_URL = 'https://data-api1.ashevillenc.gov/graphql';
-if (window.location.origin.indexOf('dev') > -1
-    || process.env.USE_DEV_API === 'true'
-    || window.location.origin.indexOf('localhost') > -1) {
-  //SIMPLICITY_SERVER_URL = 'https://dev-data-api1.ashevillenc.gov/graphql';
-  SIMPLICITY_SERVER_URL = 'simplicityapi-alb-jobs-294934993.us-east-1.elb.amazonaws.com:4000/graphql';
-}
-if (process.env.USE_LOCAL_API === 'true') {
-  SIMPLICITY_SERVER_URL = 'http://localhost:8080/graphql';
-}
+// if (window.location.origin.indexOf('dev') > -1
+//     || process.env.USE_DEV_API === 'true'
+//     || window.location.origin.indexOf('localhost') > -1) {
+//   SIMPLICITY_SERVER_URL = 'https://dev-data-api1.ashevillenc.gov/graphql';
+// }
+// if (process.env.USE_LOCAL_API === 'true') {
+//   // SIMPLICITY_SERVER_URL = 'http://localhost:8080/graphql';
+//   SIMPLICITY_SERVER_URL = 'https://dev-data-api1.ashevillenc.gov/graphql';
+
+// }
 
 const simpliCityClient = new ApolloClient({
   link: createHttpLink({
